e9086faeca4c22d8458cce6afe50ba9cdda6bd22
vinc37-git <vinc37@web.de>
2022-07-21 17:59:50 +0200

    added operators, imroved exception handling, etc..

--- src/cumat.cu
+++ src/cumat.cu
@@ -12,81 +12,313 @@
 
 #include "cumat.h"
 
+
 template <typename T>
 cuCV::CuMat<T>::CuMat() { }
 
 
 template <typename T>
-cuCV::CuMat<T>::CuMat(Mat<T> & SRC) 
-        : Mat<T>(SRC.mWidth, SRC.mHeight, SRC.mChannels, NULL) {  ///< We initialize Base Class Object "Mat" using parameters of SRC. However, dont point to the same data.
-    allocateLike(SRC);
+cuCV::CuMat<T>::CuMat(Mat<T> & mat) 
+        : Mat<T>(mat.mWidth, mat.mHeight, mat.mChannels, NULL) {  ///< We initialize Base Class Object "Mat" using parameters of mat. However, dont point to the same data.
+    //allocateLike(mat);
 }
 
 
 template <typename T>
-void cuCV::CuMat<T>::add(CuMat & OUT, CuMat & B) {
+cuCV::CuMat<T>::CuMat(int width, int height, int channels) 
+        : Mat<T>(width, height, channels, NULL) { 
+}
 
-    if (this->mData == NULL || OUT.mData == NULL || B.mData == NULL)
-        throw std::invalid_argument("INVALID POINTERS DETECTED: pointers for addition are null.");
 
-    if (this->mHeight != B.mHeight) {
-        printf("DIMENSIONS DO NOT MATCH.");
-        throw std::invalid_argument("DIMENSIONS DO NOT MATCH. @TODO CHANGE THAT");
+template <typename T>
+cuCV::CuMat<T>::CuMat(const CuMat & cuMat)
+        : Mat<T>(cuMat.mWidth, cuMat.mHeight, cuMat.mChannels, NULL) {
+    if (cuMat.mData != NULL) {
+        allocateLike(cuMat);
+        gpuErrchk(cudaMemcpy(this->mData, cuMat.mData, sizeof(T) * cuMat.mWidth * cuMat.mHeight * cuMat.mChannels, cudaMemcpyDeviceToDevice)); 
     }
+}
 
-    printf("A: x=%d, y=%d, z=%d\n", this->mWidth, this->mHeight, this->mChannels);
-    printf("B: x=%d, y=%d, z=%d\n", B.mWidth, B.mHeight, B.mChannels);
-    printf("C: x=%d, y=%d, z=%d\n", OUT.mWidth, OUT.mHeight, OUT.mChannels);
 
+template <typename T>
+cuCV::CuMat<T>::CuMat(CuMat && cuMat)
+        : Mat<T>(cuMat.mWidth, cuMat.mHeight, cuMat.mChannels, cuMat.mData) {
+    cuMat.mData = NULL;
+}
+
+
+template <typename T>
+cuCV::CuMat<T>::~CuMat() {
+    if (this->mData != NULL) {
+        cudaFree(this->mData);
+        this->mData = NULL;
+    }
+}
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator=(CuMat cuMat) {
+    std::swap(this->mData, cuMat.mData);
+    this->cuType = cuMat.cuType;
+    this->mWidth = cuMat.mWidth; 
+    this->mHeight = cuMat.mHeight; 
+    this->mStride = cuMat.mStride;
+    this->mChannels = cuMat.mChannels;
+    return * this;
+}
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator+=(const CuMat & cuMat) {
+    if (this->mData == NULL || cuMat.mData == NULL)
+        throw cuCV::exception::NullPointer("Calculation failed. One or more operands point to NULL data!");
+
+    if (!compareDim(* this, cuMat))
+        throw cuCV::exception::DimensionMismatch(* this, cuMat);
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
     const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-    const dim3 blocks((OUT.mWidth + threads.x - 1) / threads.x, (OUT.mHeight + threads.y - 1) / threads.y);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
 
-    cuCV::kernel::add<<<blocks, threads>>>(OUT, * this, B);
+    /// Perform Math
+    cuCV::kernel::add<<<blocks, threads>>>(this->kernel(), this->kernel(), cuMat.kernel());
+
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
 }
 
 
 template <typename T>
-bool cuCV::CuMat<T>::allocateLike(const Mat<T> & src) {
-    if (this->mData != NULL) {
-        /// @todo free
-        throw std::bad_alloc(); // ("LEAK-WARNING: You are trying to allocate new VRAM but mData was not NULL")
-    }
-
-    /// Allocate Memory
-    gpuErrchk(cudaMalloc((void**) & this->mData, sizeof(T) * src.mWidth * src.mHeight * src.mChannels));
-    
-
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator+=(T alpha) {
     if (this->mData == NULL)
-        return 0;
-    else 
-        return 1;
+        throw cuCV::exception::NullPointer("Calculation failed. One or more operands point to NULL data!");
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
+
+    /// Perform Math
+    cuCV::kernel::add<<<blocks, threads>>>(this->kernel(), this->kernel(), alpha);
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator+(const CuMat & cuMat) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum += cuMat;
+    return sum;
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator+(const T alpha) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum += alpha;
+    return sum;
+}
+
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator-=(const CuMat & cuMat) {
+    if (this->mData == NULL || cuMat.mData == NULL)
+        throw cuCV::exception::NullPointer("Calculation failed. One or more operands point to NULL data!");
+
+    if (!compareDim(* this, cuMat))
+        throw cuCV::exception::DimensionMismatch(* this, cuMat);
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
+
+    /// Perform Math
+    cuCV::kernel::dif<<<blocks, threads>>>(this->kernel(), this->kernel(), cuMat.kernel());
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
+}
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator-=(T alpha) {
+    if (this->mData == NULL)
+        throw cuCV::exception::NullPointer("Calculation failed. One or more operands point to NULL data!");
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
+
+    /// Perform Math
+    cuCV::kernel::dif<<<blocks, threads>>>(this->kernel(), this->kernel(), alpha);
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator-(const CuMat & cuMat) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum -= cuMat;
+    return sum;
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator-(const T alpha) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum -= alpha;
+    return sum;
+}
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator*=(const CuMat & cuMat) {
+    if (this->mData == NULL || cuMat.mData == NULL)
+        throw cuCV::exception::NullPointer("Calculation failed. One or more operands point to NULL data!");
+
+    if (!compareDim(* this, cuMat))
+        throw cuCV::exception::DimensionMismatch(* this, cuMat);
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
+
+    /// Perform Math
+    cuCV::kernel::mul<<<blocks, threads>>>(this->kernel(), this->kernel(), cuMat.kernel());
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
+}
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator*=(T alpha) {
+    if (this->mData == NULL)
+        throw cuCV::exception::NullPointer("Calculation failed. One or more operands point to NULL data!");
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
+
+    /// Perform Math
+    cuCV::kernel::mul<<<blocks, threads>>>(this->kernel(), this->kernel(), alpha);
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator*(const CuMat & cuMat) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum *= cuMat;
+    return sum;
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator*(const T alpha) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum *= alpha;
+    return sum;
+}
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator/=(const CuMat & cuMat) {
+    if (this->mData == NULL || cuMat.mData == NULL)
+        throw cuCV::exception::NullPointer("Calculation failed. One or more operands point to NULL data!");
+
+    if (!compareDim(* this, cuMat))
+        throw cuCV::exception::DimensionMismatch(* this, cuMat);
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
+
+    /// Perform Math
+    cuCV::kernel::div<<<blocks, threads>>>(this->kernel(), this->kernel(), cuMat.kernel());
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
+}
+
+
+template <typename T>
+cuCV::CuMat<T> & cuCV::CuMat<T>::operator/=(T alpha) {
+    if (this->mData == NULL)
+        throw cuCV::exception::NullPointer("PointerError: one or more operands point to NULL data!");
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((this->mWidth + threads.x - 1) / threads.x, (this->mHeight + threads.y - 1) / threads.y, this->mChannels);
+
+    /// Perform Math
+    cuCV::kernel::div<<<blocks, threads>>>(this->kernel(), this->kernel(), alpha);
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return * this;    
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator/(const CuMat & cuMat) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum /= cuMat;
+    return sum;
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::CuMat<T>::operator/(const T alpha) const {
+    cuCV::CuMat<T> sum(*this);  // Copy `this`
+    sum /= alpha;
+    return sum;
 }
 
 
 template <typename T>
 void cuCV::CuMat<T>::uploadFrom(const Mat<T> & src) {
-    /// Check wether 'this' contains already data. 
-    /// If yes, the dimensions must match. If no, allocate, set dimensions and data.
-    if (this->mData == NULL) {
-        if (!allocateLike(src))
-            throw std::bad_alloc(); //("Allocation of VRAM failed.")
-        
+    /** Workflow
+     * 2. Check dimensions. If uniintialised on device, initialise. If mismatch, throw error.
+     * 3. Allocate MEM for device matrix. Dimensions checks makes sure count is right.
+     * 4. Transfer data.
+     */
+
+    // Check if dimenions match. If dimensions are uninitialized assign them.
+    if ((this->mWidth == 0) || (this->mHeight == 0) || (this->mChannels == 0)) {
         this->mWidth = src.mWidth;
         this->mHeight = src.mHeight;
         this->mChannels = src.mChannels;
         this->mStride = src.mStride;
     }
-    else {  ///< Data is not null, so check dimensions.
-        if (this->mWidth != src.mWidth || this->mHeight != src.mHeight ||
-            this->mChannels != src.mChannels || this->mStride != src.mStride) 
-            throw std::invalid_argument("DIMENSIONS DO NOT MATCH. @TODO CHANGE THAT");
-        else {
-            printf("'this' points to data. Please free data before allocating new data.");
-            //abort();
-        }
+    else if (!compareDim(src, * this)) {
+        throw cuCV::exception::DimensionMismatch(src, * this);
     }
+
+    /// Allocate Memory on device in 'this' points to NULL. If it points to data, it will throw an exception.
+    if (this->mData == NULL)
+        allocateLike(src);
 
     /// Send Memory from src to 'this'. 'this' is on device
     gpuErrchk(cudaMemcpy(this->mData, src.mData, sizeof(T) * src.mWidth * src.mHeight * src.mChannels, cudaMemcpyHostToDevice));   
@@ -94,24 +326,132 @@ void cuCV::CuMat<T>::uploadFrom(const Mat<T> & src) {
 
 
 template <typename T>
-void cuCV::CuMat<T>::downloadTo(const Mat<T> & dst) const {
+void cuCV::CuMat<T>::downloadTo(Mat<T> & dst) const {
+    /** Workflow
+     * 1. Check if CuMat points to data
+     * 2. Check dimensions. If uniintialised, initialised. If mismatch, throw error.
+     * 3. Allocate MEM for host matrix. Dimensions checks makes sure count is right.
+     * 4. Transfer data.
+     */
+
+    if (this->mData == NULL)
+        throw cuCV::exception::NullPointer("Download failed. mData of source on device is NULL"); ///< @note undefined behaviour! 
+
+    // Check if dimenions match. If dimensions are uninitialized assign them.
+    if ((dst.mWidth == 0) || (dst.mHeight == 0) || (dst.mChannels == 0)) {
+        dst.mWidth = this->mWidth;
+        dst.mHeight = this->mHeight;
+        dst.mChannels = this->mChannels;
+        dst.mStride = this->mStride;
+    }
+    else if (!compareDim(* this, dst)) {
+        throw cuCV::exception::DimensionMismatch(* this, dst);
+    }
+
+    // If MEM for destination on host is not allocated yet, allocate.
     if (dst.mData == NULL) {
-        throw std::invalid_argument("mData of destination is NULL");  ///< @note undefined behaviour! 
+        dst.alloc();  // The dim check makes sure the allocated size is always the right one for `this`.
     }
-    if (this->mData == NULL) {
-        throw std::invalid_argument("mData of source on device is NULL"); ///< @note undefined behaviour! 
-    }
+
     gpuErrchk(cudaMemcpy(dst.mData, this->mData, sizeof(T) * dst.mWidth * dst.mHeight * dst.mChannels, cudaMemcpyDeviceToHost));
 }
 
 
 template <typename T>
-void cuCV::CuMat<T>::free() {
+void cuCV::CuMat<T>::allocateLike(const Mat<T> & src) {
+    // Check if dimenions match. If dimensions are uninitialized assign them.
+    if ((this->mWidth == 0) || (this->mHeight == 0) || (this->mChannels == 0)) {
+        this->mWidth = src.mWidth;
+        this->mHeight = src.mHeight;
+        this->mChannels = src.mChannels;
+        this->mStride = src.mStride;
+    }
+    else if (!compareDim(src, * this))
+        throw cuCV::exception::DimensionMismatch(src, * this, "allocation");
 
+    if (this->mData != NULL) {
+        fprintf(stderr, "WARNING: Data on device is freed automatically when function allocateLike was called. Make sure to clear data yourself.");
+        clearOnDevice();
+    }
+
+    /// Allocate Memory
+    gpuErrchk(cudaMalloc((void**) & this->mData, sizeof(T) * src.mWidth * src.mHeight * src.mChannels));
+    
+    if (this->mData == NULL)
+        throw std::bad_alloc(); //("Allocation of VRAM failed.")
+}
+
+
+template <typename T>
+void cuCV::CuMat<T>::allocateOnDevice() {
+    if (this->mData != NULL) {
+        fprintf(stderr, "WARNING: Data on device is freed automatically when function allocateLike was called. Make sure to clear data yourself.");
+        clearOnDevice();
+    }
+    if ((this->mWidth == 0) || (this->mHeight == 0) || (this->mChannels == 0))
+        throw std::bad_alloc();  ///
+
+    gpuErrchk(cudaMalloc((void**) & this->mData, sizeof(T) * this->mWidth * this->mHeight * this->mChannels));
+    
+    if (this->mData == NULL)
+        throw std::bad_alloc(); //("Allocation of VRAM failed.")    
+}
+
+
+template <typename T>
+void cuCV::CuMat<T>::clearOnDevice() {
+    if (this->mData != NULL) {
+        cudaFree(this->mData);
+        this->mData = NULL;
+    }
+}
+
+
+template <typename T>
+cuCV::KernelCuMat<T> cuCV::CuMat<T>::kernel() const {
+    return cuCV::KernelCuMat<T>(* this);
+}
+
+
+template <typename T>
+bool cuCV::CuMat<T>::compareDim(const CuMat & A, const CuMat & B) const {
+    if (A.mWidth != B.mWidth || A.mHeight != B.mHeight || A.mChannels != B.mChannels)
+        return 0;
+    return 1;
+}
+
+template <typename T>
+bool cuCV::CuMat<T>::compareDim(const CuMat & A, const cuCV::Mat<T> & B) const {
+    if (A.mWidth != B.mWidth || A.mHeight != B.mHeight || A.mChannels != B.mChannels)
+        return 0;
+    return 1;
+}
+
+template <typename T>
+bool cuCV::CuMat<T>::compareDim(const cuCV::Mat<T> & A, const CuMat & B) const {
+    if (A.mWidth != B.mWidth || A.mHeight != B.mHeight || A.mChannels != B.mChannels)
+        return 0;
+    return 1;
+}
+
+template <typename T>
+bool cuCV::CuMat<T>::compareDim(const cuCV::Mat<T> & A, const cuCV::Mat<T> & B) const {
+    if (A.mWidth != B.mWidth || A.mHeight != B.mHeight || A.mChannels != B.mChannels)
+        return 0;
+    return 1;
+}
+
+
+template <typename T>
+bool cuCV::CuMat<T>::empty() const {
+    if (this->mData == NULL)
+        return true;
+    else
+        return false;
 }
 
 
 /// Explicit template specialization
-template class cuCV::CuMat<unsigned char>;
-template class cuCV::CuMat<unsigned short>;
-template class cuCV::CuMat<double>;
+template class cuCV::CuMat<CUCV_8U>;
+template class cuCV::CuMat<CUCV_16U>;
+template class cuCV::CuMat<CUCV_64F>;
--- src/cumat.h
+++ src/cumat.h
@@ -13,38 +13,200 @@
 #ifndef CUMAT_H
 #define CUMAT_H
 
-
-
 #include <iostream>
 #include <unistd.h>
-
 
 #include "errorhandling.h"
 #include "mat.h"
 #include "kernel.h"
+#include "kernelcumat.h"
 
-#define BLOCK_SIZE 5
+#define BLOCK_SIZE 32
 
 
 namespace cuCV {
 
+/**
+ * @brief The CuMat class is inherited from the basic Mat class and represents a mathematical matrix 
+ * with a maximum of 3 dimensions on a cuda device in cuCV. It can be used to perform mathematical operations
+ * on the device using cuda kernels. Public and protected member variables are inherited from the base class 
+ * and provide informations about the dimensions of the matrix. Note that the developer must take care of both 
+ * data transfer AND data deletion, meaning data is not automatically deleted from the device after the download to the host.
+ * This allows data reading on the Host while it can still be used for further calculations on device. However, data Allocation
+ * on device is usually done before data transfer automatically.
+ * 
+ * @tparam T 
+ */
 template <typename T>
 class CuMat : public Mat<T> {
 public:
-
+    /**
+     * @brief Construct a new CuMat object using the standard constructor. Using this constructor 
+     * will require setting of dimensions and/or data pointer in a subsequent step.
+     */
     CuMat();
-    CuMat(Mat<T> & SRC);
+
+
+    /**
+     * @brief Construct a new CuMat object from a Mat Object on the host. Dimension parameters will be assigned
+     * automatically. However, no data will be allocated on the device. 
+     * @param mat 
+     */
+    CuMat(Mat<T> & mat);
+
+
+    CuMat(int width, int height, int channels);
+
+
+    /**
+     * @brief Construct a new CuMat object by copying another CuMat Object on the device.
+     * 
+     * @param cuMat 
+     */
+    CuMat(const CuMat & cuMat);
+
+
+    /**
+     * @brief Construct a new CuMat object by moving a source CuMat Object on the device.
+     * 
+     * @param cuMat 
+     */
+    CuMat(CuMat && cuMat);
+    
+    
     //CuMat(int width, int height, int channels, void * data);
 
-    void add(CuMat & OUT, CuMat & B);
+
+    /**
+     * @brief Destroy the Cu Mat object. If the object points to memory on device, this data will be freed.
+     */
+    ~CuMat();
+
+
+    // ******* OPERATORS **********
+
+    /**
+     * @brief Copy Assigment operator.
+     * 
+     * @param cuMat 
+     * @return a copy of cuMat
+     */
+    CuMat & operator=(CuMat cuMat);
+
+
+    /**
+     * @brief Add one CuMat object to the other elementwise and assign it to itself. 
+     * 
+     * @param cuMat 
+     * @return CuMat& 
+     */
+    CuMat & operator+=(const CuMat & cuMat);
+    CuMat & operator+=(const T alpha);
+
+
+    /**
+     * @brief Add one CuMat object to the other elementwise.
+     * 
+     * @param cuMat 
+     * @return CuMat 
+     */
+    CuMat operator+(const CuMat & cuMat) const;
+    CuMat operator+(const T alpha) const;
+
+
+    /**
+     * @brief Subtract one CuMat object to the other elementwise and assign it to itself. 
+     * 
+     * @param cuMat 
+     * @return CuMat& 
+     */
+    CuMat & operator-=(const CuMat & cuMat);
+    CuMat & operator-=(const T alpha);
+
+
+    /**
+     * @brief Subtract one CuMat object to the other elementwise.
+     * 
+     * @param cuMat 
+     * @return CuMat 
+     */
+    CuMat operator-(const CuMat & cuMat) const;
+    CuMat operator-(const T alpha) const;
+
+
+    /**
+     * @brief Multiply one CuMat object with the other elementwise and assign it to itself. 
+     * 
+     * @param cuMat 
+     * @return CuMat& 
+     */
+    CuMat & operator*=(const CuMat & cuMat);
+    CuMat & operator*=(const T alpha);
+
+
+    /**
+     * @brief Multiply one CuMat object to the other elementwise.
+     * 
+     * @param cuMat 
+     * @return CuMat 
+     */
+    CuMat operator*(const CuMat & cuMat) const;
+    CuMat operator*(const T alpha) const;
+
+
+    /**
+     * @brief Divide one CuMat object with the other elementwise and assign it to itself. 
+     * 
+     * @param cuMat 
+     * @return CuMat& 
+     */
+    CuMat & operator/=(const CuMat & cuMat);
+    CuMat & operator/=(const T alpha);
+
+
+    /**
+     * @brief Divide one CuMat object with the other elementwise.
+     * 
+     * @param cuMat 
+     * @return CuMat 
+     */
+    CuMat operator/(const CuMat & cuMat) const;
+    CuMat operator/(const T alpha) const;
+
+
+    // ******* DATA MANAGEMENT METHODS **********
+
 
     void uploadFrom(const Mat<T> & srcMat);
-    void downloadTo(const Mat<T> & dstMat) const;
+    void downloadTo(Mat<T> & dstMat) const;
 
-private:
-    bool allocateLike(const Mat<T> & srcMat);
-    void free();
+    void allocateOnDevice();
+    void allocateLike(const Mat<T> & srcMat);
+    void clearOnDevice();
 
+    KernelCuMat<T> kernel() const;
+
+
+//private:
+    /**
+     * @brief Compare Dimenions of Matrix A and B on Equality.
+     * 
+     * @param A Matrix A
+     * @param B Matrix B
+     * @return true if dimensions are equal. Otherwise false.
+     */
+    bool compareDim(const CuMat & A, const CuMat & B) const;
+    bool compareDim(const CuMat & A, const Mat<T> & B) const;
+    bool compareDim(const Mat<T> & A, const CuMat & B) const;
+    bool compareDim(const Mat<T> & A, const Mat<T> & B) const;
+
+
+    /**
+     * @brief Check if Matrix points to data.
+     * 
+     * @return true if data pointer is null.
+     */
+    bool empty() const;
 };
 
 };
