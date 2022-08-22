332faab2ef558ef0e62c98beb931c04e5414bd9a
vinc37-git <vinc37@web.de>
2022-08-22 16:06:27 +0200

    added cudaDeviceReset() to cppunit testing created main testing file for all tests

--- tests/unitTestMain.cpp
+++ tests/unitTestMain.cpp
@@ -1,7 +1,8 @@
-
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+
+#include "errorhandling.h"
 
 int main() {
     // Get the top level suite from the registry
@@ -10,5 +11,8 @@ int main() {
     CppUnit::TextUi::TestRunner runner;
     runner.addTest(suite);
     bool wasSucessful = runner.run();
+
+    gpuErrchk(cudaDeviceReset());  // to detect leaks with cuda-memcheck we need to call cudaDeviceReset(). 
+
     return wasSucessful ? 0 : 1;
 }
