6f57234ba6d725b4f9edcfdb11b87ec5b9f69f1d
vinc37-git <vinc37@web.de>
2022-08-31 13:19:29 +0200

    added in-place operations

--- src/linalg.cu
+++ src/linalg.cu
@@ -13,89 +13,178 @@
 
 
 template <typename T>
-cuCV::CuMat<T> cuCV::naiveMatmul(const cuCV::CuMat<T> & A, const cuCV::CuMat<T> & B) {
+void cuCV::simpleMatmul(cuCV::CuMat<T> & OUT, const cuCV::CuMat<T> & A, const cuCV::CuMat<T> & B) {
     // Matrices must have a shape that matches the signature A(m,n) @ B(n,l)->(m,l)
     if (A.mWidth != B.mHeight || A.mChannels != B.mChannels)
-        throw cuCV::exception::DimensionMismatch(A, B, "naive matmul operation");
+        throw cuCV::exception::DimensionMismatch(A, B, "matmul operation");
+
+    if (OUT.mHeight != A.mHeight || OUT.mWidth != B.mWidth)
+        throw std::runtime_error("DimensionMismatch: Matrices must have a shape that matches the signature A(m,n) @ B(n,l)->(m,l).");
     
     if (A.mData == NULL || B.mData == NULL)
         throw cuCV::exception::NullPointer("PointerError: one or more operands point to NULL data!");
 
-    cuCV::CuMat<T> out(A.mHeight, B.mWidth, A.mChannels);
-    out.allocateOnDevice();
+    if (OUT.getDataPtr() == NULL)
+        OUT.allocateOnDevice();
 
     // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
     const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-    const dim3 blocks((out.mWidth + threads.x - 1) / threads.x, (out.mHeight + threads.y - 1) / threads.y, out.mChannels);
+    const dim3 blocks((OUT.mWidth + threads.x - 1) / threads.x, (OUT.mHeight + threads.y - 1) / threads.y, OUT.mChannels);
 
     /// Perform Math
-    cuCV::kernel::naiveMatmul<<<blocks, threads>>>(out.kernel(), A.kernel(), B.kernel());
+    cuCV::kernel::naiveMatmul<<<blocks, threads>>>(OUT.kernel(), A.kernel(), B.kernel());
 
-    return out;
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+}
+
+
+template <typename T>
+cuCV::CuMat<T> cuCV::naiveMatmul(const cuCV::CuMat<T> & A, const cuCV::CuMat<T> & B) {
+    cuCV::CuMat<T> OUT(A.mHeight, B.mWidth, A.mChannels);
+    OUT.allocateOnDevice();
+
+    cuCV::simpleMatmul(OUT, A, B);
+
+    return OUT;
+}
+
+
+template <typename T>
+void cuCV::matmul(cuCV::CuMat<T> & OUT, const cuCV::CuMat<T> & A, const cuCV::CuMat<T> & B) {
+    // Matrices must have a shape that matches the signature A(m,n) @ B(n,l)->(m,l)
+    if (A.mWidth != B.mHeight || A.mChannels != B.mChannels)
+        throw cuCV::exception::DimensionMismatch(A, B, "matmul operation");
+
+    if (OUT.mHeight != A.mHeight || OUT.mWidth != B.mWidth)
+        throw std::runtime_error("DimensionMismatch: Matrices must have a shape that matches the signature A(m,n) @ B(n,l)->(m,l).");
+    
+    if (A.mData == NULL || B.mData == NULL)
+        throw cuCV::exception::NullPointer("PointerError: one or more operands point to NULL data!");
+
+    if (OUT.getDataPtr() == NULL)
+        OUT.allocateOnDevice();
+
+    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
+    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+    const dim3 blocks((OUT.mWidth + threads.x - 1) / threads.x, (OUT.mHeight + threads.y - 1) / threads.y, OUT.mChannels);
+
+    /// Perform Math
+    cuCV::kernel::matmul<<<blocks, threads>>>(OUT.kernel(), A.kernel(), B.kernel());
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
 }
 
 
 template <typename T>
 cuCV::CuMat<T> cuCV::matmul(const cuCV::CuMat<T> & A, const cuCV::CuMat<T> & B) {
-    // Matrices must have a shape that matches the signature A(m,n) @ B(n,l)->(m,l)
-    if (A.mWidth != B.mHeight || A.mChannels != B.mChannels)
-        throw cuCV::exception::DimensionMismatch(A, B, "matmul operation");
-    
-    if (A.mData == NULL || B.mData == NULL)
+
+    cuCV::CuMat<T> OUT(A.mHeight, B.mWidth, A.mChannels);
+    OUT.allocateOnDevice();
+
+    cuCV::matmul(OUT, A, B);
+
+    return OUT;
+}
+
+
+template <typename T1, typename T2> 
+void cuCV::slowConv2d(CuMat<T1> & OUT, const CuMat<T1> & A, const CuMat<T2> & kernel, const cuCV::Padding padding) {
+
+    if (A.getDataPtr() == NULL || kernel.getDataPtr() == NULL)
         throw cuCV::exception::NullPointer("PointerError: one or more operands point to NULL data!");
 
-    cuCV::CuMat<T> out(A.mHeight, B.mWidth, A.mChannels);
-    out.allocateOnDevice();
+    if (A.getWidth() < kernel.getWidth() || A.getHeight() < kernel.getHeight()) {
+        std::string msg = "DimensionError: kernel size is larger than matrix size: (" 
+                + std::to_string(A.getWidth()) + ", " + std::to_string(A.getHeight()) + ") vs ("
+                + std::to_string(kernel.getWidth()) + ", " + std::to_string(kernel.getHeight()) + ")";
+        throw std::runtime_error(msg);
+    }
+
+    // Determine size of output matrix
+    int outW = 0, outH = 0;
+    switch (padding) {
+        case cuCV::Padding::ZERO:
+            outW = A.getWidth(), outH = A.getHeight();
+            break;
+        default:
+            break;
+    }
+
+    // Check if OUT matches determined size
+
+    // Check if OUT points to data
+    if (OUT.getDataPtr() == NULL)
+        OUT.allocateOnDevice();
 
     // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
     const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-    const dim3 blocks((out.mWidth + threads.x - 1) / threads.x, (out.mHeight + threads.y - 1) / threads.y, out.mChannels);
+    const dim3 blocks((OUT.mWidth + threads.x - 1) / threads.x, (OUT.mHeight + threads.y - 1) / threads.y, OUT.mChannels);
 
-    cuCV::kernel::matmul<<<blocks, threads>>>(out.kernel(), A.kernel(), B.kernel());
-    
-    return out;
+    cuCV::kernel::slowConv2d<<<blocks, threads>>>(OUT.kernel(), A.kernel(), kernel.kernel(), padding);
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
 }
 
 
 template <typename T1, typename T2> 
 cuCV::CuMat<T1> cuCV::slowConv2d(const CuMat<T1> & A, const CuMat<T2> & kernel, const cuCV::Padding padding) {
-/// @todo What happens when the kernel size is larger than A?
-
-    if (A.getDataPtr() == NULL || kernel.getDataPtr() == NULL)
-        throw cuCV::exception::NullPointer("PointerError: one or more operands point to NULL data!");
 
     // Determine size of output matrix
     int outW = 0, outH = 0;
     switch (padding) {
-    case cuCV::Padding::ZERO :
-        outW = A.getWidth(), outH = A.getHeight();
-        break;
-    default:
-        break;
+        case cuCV::Padding::ZERO:
+            outW = A.getWidth(), outH = A.getHeight();
+            break;
+        default:
+            break;
     }
 
     // Create output matrix
     cuCV::CuMat<T1> out(outH, outW, A.getNChannels());
     out.allocateOnDevice();
 
-    // Construct Grid. As for images usually cols && rows >> nCh we do not launch a whole thread-block in z dimension.
-    const dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-    const dim3 blocks((out.mWidth + threads.x - 1) / threads.x, (out.mHeight + threads.y - 1) / threads.y, out.mChannels);
-
-    cuCV::kernel::slowConv2d<<<blocks, threads>>>(out.kernel(), A.kernel(), kernel.kernel(), padding);
+    cuCV::slowConv2d(out, A, kernel, padding);
 
     return out;
 }
 
 
+// template <typename T>
+// cuCV::CuMat<T> cuCV::slowConv2d(const CuMat<T> & A, const cuCV::Kernel kerneltype, const size_t kernelX, const size_t kernelY, const cuCV::Padding padding) {
+//     // Create kernel
+    
+// }
+
+
 /// Explicit template specialization
+template void cuCV::simpleMatmul(cuCV::CuMat<CUCV_8U> & OUT, const cuCV::CuMat<CUCV_8U> & A, const cuCV::CuMat<CUCV_8U> & B);
+template void cuCV::simpleMatmul(cuCV::CuMat<CUCV_16U> & OUT, const cuCV::CuMat<CUCV_16U> & A, const cuCV::CuMat<CUCV_16U> & B);
+template void cuCV::simpleMatmul(cuCV::CuMat<CUCV_64F> & OUT, const cuCV::CuMat<CUCV_64F> & A, const cuCV::CuMat<CUCV_64F> & B);
+
 template cuCV::CuMat<CUCV_8U> cuCV::naiveMatmul(const cuCV::CuMat<CUCV_8U> & A, const cuCV::CuMat<CUCV_8U> & B);
 template cuCV::CuMat<CUCV_16U> cuCV::naiveMatmul(const cuCV::CuMat<CUCV_16U> & A, const cuCV::CuMat<CUCV_16U> & B);
 template cuCV::CuMat<CUCV_64F> cuCV::naiveMatmul(const cuCV::CuMat<CUCV_64F> & A, const cuCV::CuMat<CUCV_64F> & B);
 
+template void cuCV::matmul(cuCV::CuMat<CUCV_8U> & OUT, const cuCV::CuMat<CUCV_8U> & A, const cuCV::CuMat<CUCV_8U> & B);
+template void cuCV::matmul(cuCV::CuMat<CUCV_16U> & OUT, const cuCV::CuMat<CUCV_16U> & A, const cuCV::CuMat<CUCV_16U> & B);
+template void cuCV::matmul(cuCV::CuMat<CUCV_64F> & OUT, const cuCV::CuMat<CUCV_64F> & A, const cuCV::CuMat<CUCV_64F> & B);
+
 template cuCV::CuMat<CUCV_8U> cuCV::matmul(const cuCV::CuMat<CUCV_8U> & A, const cuCV::CuMat<CUCV_8U> & B);
 template cuCV::CuMat<CUCV_16U> cuCV::matmul(const cuCV::CuMat<CUCV_16U> & A, const cuCV::CuMat<CUCV_16U> & B);
 template cuCV::CuMat<CUCV_64F> cuCV::matmul(const cuCV::CuMat<CUCV_64F> & A, const cuCV::CuMat<CUCV_64F> & B);
+
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_8U> & OUT, const CuMat<CUCV_8U> & A, const CuMat<CUCV_8U> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_16U> & OUT, const CuMat<CUCV_16U> & A, const CuMat<CUCV_8U> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_64F> & OUT, const CuMat<CUCV_64F> & A, const CuMat<CUCV_8U> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_8U> & OUT, const CuMat<CUCV_8U> & A, const CuMat<CUCV_16U> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_16U> & OUT, const CuMat<CUCV_16U> & A, const CuMat<CUCV_16U> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_64F> & OUT, const CuMat<CUCV_64F> & A, const CuMat<CUCV_16U> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_8U> & OUT, const CuMat<CUCV_8U> & A, const CuMat<CUCV_64F> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_16U> & OUT, const CuMat<CUCV_16U> & A, const CuMat<CUCV_64F> & kernel, const cuCV::Padding padding);
+template void cuCV::slowConv2d(cuCV::CuMat<CUCV_64F> & OUT, const CuMat<CUCV_64F> & A, const CuMat<CUCV_64F> & kernel, const cuCV::Padding padding);
 
 template cuCV::CuMat<CUCV_8U> cuCV::slowConv2d(const CuMat<CUCV_8U> & A, const CuMat<CUCV_8U> & kernel, const cuCV::Padding padding);
 template cuCV::CuMat<CUCV_16U> cuCV::slowConv2d(const CuMat<CUCV_16U> & A, const CuMat<CUCV_8U> & kernel, const cuCV::Padding padding);
@@ -106,3 +195,5 @@ template cuCV::CuMat<CUCV_64F> cuCV::slowConv2d(const CuMat<CUCV_64F> & A, const
 template cuCV::CuMat<CUCV_8U> cuCV::slowConv2d(const CuMat<CUCV_8U> & A, const CuMat<CUCV_64F> & kernel, const cuCV::Padding padding);
 template cuCV::CuMat<CUCV_16U> cuCV::slowConv2d(const CuMat<CUCV_16U> & A, const CuMat<CUCV_64F> & kernel, const cuCV::Padding padding);
 template cuCV::CuMat<CUCV_64F> cuCV::slowConv2d(const CuMat<CUCV_64F> & A, const CuMat<CUCV_64F> & kernel, const cuCV::Padding padding);
+
+// template cuCV::CuMat<CUCV_8U> cuCV::slowConv2d(const CuMat<CUCV_8U> & A, const cuCV::Kernel kerneltype, const size_t kernelX, const size_t kernelY, const cuCV::Padding padding);
--- src/linalg.h
+++ src/linalg.h
@@ -27,7 +27,21 @@ namespace cuCV {
 
 /**
  * @brief Calculate the matrix multiplication of two matrices on the device 
- * without using the concept of shared memory.
+ * without using the concept of shared memory. 
+ * Pass the matrix which will keep the result by reference.
+ * 
+ * @tparam T CUCV datatype: CUCV_8U, CUCV_16U, CUCV_64F
+ * @param OUT The matrix which will hold the resulting matrix of shape NxL.
+ * @param A The first operand with shape NxM.
+ * @param B The second operand with shape MxL.
+ */
+template <typename T>
+void simpleMatmul(CuMat<T> & OUT, const CuMat<T> & A, const CuMat<T> & B);
+
+
+/**
+ * @brief Calculate the matrix multiplication of two matrices on the device 
+ * without using the concept of shared memory and return the result.
  * @todo rename to simpleMatmul()
  * 
  * @tparam T CUCV datatype: CUCV_8U, CUCV_16U, CUCV_64F
@@ -41,7 +55,22 @@ CuMat<T> naiveMatmul(const CuMat<T> & A, const CuMat<T> & B);
 
 /**
  * @brief Calculate the matrix multiplication of two matrices on the device 
- * using the concept of shared memory.
+ * using the concept of shared memory. 
+ * Pass the matrix which will keep the result by reference.
+ * @todo some words about shared memory.
+ * 
+ * @tparam T CUCV datatype: CUCV_8U, CUCV_16U, CUCV_64F
+ * @param OUT The matrix which will hold the resulting matrix of shape NxL.
+ * @param A The first operand with shape NxM.
+ * @param B The second operand with shape MxL.
+ */
+template <typename T>
+void matmul(CuMat<T> & OUT, const CuMat<T> & A, const CuMat<T> & B);
+
+
+/**
+ * @brief Calculate the matrix multiplication of two matrices on the device 
+ * using the concept of shared memory and return the result.
  * @todo some words about shared memory.
  * 
  * @tparam T CUCV datatype: CUCV_8U, CUCV_16U, CUCV_64F
@@ -63,9 +92,27 @@ CuMat<T> slowConv2d(const CuMat<T> & A, const cuCV::Kernel kernel, const size_t 
 /**
  * @brief Perform a convolution on `A` by passing a kernel. `A` can be padded according to Padding enum.
  * The calculation is performed by the simplest convolution method.
+ * Pass the matrix which will keep the result by reference.
  * @todo rename to simpleConv2d
  * 
  * @tparam T1 CUCV datatype of `OUT` and `A`: CUCV_8U, CUCV_16U, CUCV_64F.
+ * @tparam T2 CUCV datatype of the kernel: CUCV_8U, CUCV_16U, CUCV_64F.
+ * @param OUT The matrix which will keep the result.
+ * @param A The Matrix to be convoluted.
+ * @param kernel The kernel for the convolution. Note that the kernel will be flipped (since it is a convolution and not correlation).
+ * @param padding The padding method.
+ * @return the resulting matrix of type T1. The size dependes on the Padding method.
+ */
+template <typename T1, typename T2>
+void slowConv2d(CuMat<T1> & OUT, const CuMat<T1> & A, const CuMat<T2> & kernel, const cuCV::Padding padding);
+
+
+/**
+ * @brief Perform a convolution on `A` by passing a kernel. `A` can be padded according to Padding enum.
+ * The calculation is performed by the simplest convolution method.
+ * @todo rename to simpleConv2d
+ * 
+ * @tparam T1 CUCV datatype of return and `A`: CUCV_8U, CUCV_16U, CUCV_64F.
  * @tparam T2 CUCV datatype of the kernel: CUCV_8U, CUCV_16U, CUCV_64F.
  * @param A The Matrix to be convoluted.
  * @param kernel The kernel for the convolution. Note that the kernel will be flipped (since it is a convolution and not correlation).
