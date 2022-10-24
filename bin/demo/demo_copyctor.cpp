c6586b89a2fec8281523aee1df864a2f24c2db69
vinc37-git <vinc37@web.de>
2022-10-24 22:00:18 +0200

    demo fix

--- bin/demo/demo_copyctor.cpp
+++ bin/demo/demo_copyctor.cpp
@@ -1,11 +1,13 @@
 #include <mat.h>
 #include <initializers.h>
+
 int main() {
     // Scenario, if no copy-constructor and copy-assignment operator was defined,
     // but destructor automatically deletes its data.
     cuCV::Mat<CUCV_32F> A = cuCV::ones<CUCV_32F>(128,64,3);
     {
-        cuCV::Mat<CUCV_32F> B = A;  // Would not copy data of A, but only pointer
+        //cuCV::Mat<CUCV_32F> B = A;  // Would not copy data of A, but only pointer like the expression below
+        cuCV::Mat<CUCV_32F> B(A.getWidth(), A.getHeight(), A.getNChannels(), A.getDataPtr(), false);  // Would not copy data of A, but only pointer
     }  // destroys B. Frees data of B (and A)
-    A += 1;  // Would raise cuCV::exception::NullPointer
+    A += 1;  // Would raise double free
 }
